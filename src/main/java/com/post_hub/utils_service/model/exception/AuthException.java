45a196f77fb4ee42de37a7dae19ec2fad39e695b
Alexandru <besliualexandru33@gmail.com>
2025-12-11 20:42:06 +0200

    first commit

---
+++ src/main/java/com/post_hub/utils_service/model/exception/AuthException.java
@@ -0,0 +1,9 @@
+package com.post_hub.utils_service.model.exception;
+
+public class AuthException extends RuntimeException {
+
+    public AuthException(String message) {
+        super(message);
+    }
+
+}
