26fb068230c266a347aa7a08fe11ca8caff51e5a
Alexandru <besliualexandru33@gmail.com>
2025-12-11 21:26:03 +0200

    Add ActionLog entity and supporting enums for logging actions

---
+++ src/main/java/com/post_hub/utils_service/utils/enum_converter/PostHubServiceConverter.java
@@ -0,0 +1,16 @@
+package com.post_hub.utils_service.utils.enum_converter;
+
+import com.post_hub.utils_service.model.enums.PostHubService;
+import jakarta.persistence.AttributeConverter;
+
+public class PostHubServiceConverter implements AttributeConverter<PostHubService, String> {
+    @Override
+    public String convertToDatabaseColumn(PostHubService attribute) {
+        return attribute.getValue();
+    }
+
+    @Override
+    public PostHubService convertToEntityAttribute(String dbData) {
+        return PostHubService.fromValue(dbData);
+    }
+}
